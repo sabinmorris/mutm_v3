55f4ed4f6bc2b1d9e09e9e362d45f2189c906b22
Elias morris <sabinmorris8@gmail.com>
2025-10-08 10:23:50 +0300

    increase pagination in license controller

--- license_controller/index.php
+++ license_controller/index.php
@@ -103,7 +103,7 @@
                     <div class="card-body table-responsive p-0" id="notPaidSearchDiv">
                         <?php
 
-                        $json = file_get_contents($pubIP1 . 'mutm/api/getLicenseByStatus/false?pageNum=1&pageSize=200');
+                        $json = file_get_contents($pubIP1 . 'mutm/api/getLicenseByStatus/false?pageNum=1&pageSize=500');
                         $arr = json_decode($json, true); //covert json data into array format
 
                         include('licenseList.php');
